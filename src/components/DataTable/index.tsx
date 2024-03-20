e881f3080373baa25975001083b555825a875012
Javier Jael Castillo <javier_jave@hotmail.com>
2024-03-19 18:18:17 -0600

    update 24.2

--- src/components/DataTable/index.tsx
+++ src/components/DataTable/index.tsx
@@ -1,5 +1,6 @@
 import {
   Paper,
+  Stack,
   Table,
   TableBody,
   TableCell,
@@ -12,7 +13,23 @@ import React from "react";
 import useProcessData from "../../hooks/useProcessData";
 
 const DataTable = () => {
-  const { processedAllDataWithFinalScore } = useProcessData();
+  const { processedAllDataWithFinalScore, processedData241, processedData242 } =
+    useProcessData();
+
+  const getOrdinalSuffix = (i: number) => {
+    const j = i % 10;
+    const k = i % 100;
+    if (j === 1 && k !== 11) {
+      return i + "st";
+    }
+    if (j === 2 && k !== 12) {
+      return i + "nd";
+    }
+    if (j === 3 && k !== 13) {
+      return i + "rd";
+    }
+    return i + "th";
+  };
 
   return (
     <TableContainer component={Paper}>
@@ -20,27 +37,42 @@ const DataTable = () => {
         <TableHead sx={{ backgroundColor: "#FBBC41" }}>
           <TableRow>
             <TableCell align="center">
-              <Typography>Rank</Typography>
+              <Typography fontWeight={600}>Rank</Typography>
             </TableCell>
             <TableCell>
-              <Typography>Nombre</Typography>
+              <Typography fontWeight={600}>Nombre</Typography>
             </TableCell>
             <TableCell>
-              <Typography>24.1</Typography>
+              <Typography fontWeight={600}>24.1</Typography>
             </TableCell>
             <TableCell>
-              <Typography>24.2</Typography>
+              <Typography fontWeight={600}>24.2</Typography>
             </TableCell>
             <TableCell>
-              <Typography>24.3</Typography>
+              <Typography fontWeight={600}>24.3</Typography>
             </TableCell>
             <TableCell>
-              <Typography>Score</Typography>
+              <Typography fontWeight={600}>Final</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography fontWeight={600}>Score</Typography>
             </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {processedAllDataWithFinalScore.map((row, i) => {
+            const data24_1 = processedData241.find(
+              (pd) => pd.name === row.name
+            );
+            const place24_1 = processedData241.findIndex(
+              (pd) => pd.name === row.name
+            );
+            const data24_2 = processedData242.find(
+              (pd) => pd.name === row.name
+            );
+            const place24_2 = processedData242.findIndex(
+              (pd) => pd.name === row.name
+            );
             return (
               <TableRow
                 key={row.name}
@@ -50,20 +82,45 @@ const DataTable = () => {
                 }}
               >
                 <TableCell component="th" scope="row" align="center">
-                  <Typography>{i + 1}</Typography>
+                  <Typography
+                    variant={i < 3 ? "h5" : "body1"}
+                    fontWeight={i < 3 ? 600 : 500}
+                  >
+                    {i + 1}
+                  </Typography>
+                  <Typography>({row.finalScore} pts)</Typography>
                 </TableCell>
                 <TableCell>
-                  <Typography>{row.name}</Typography>
+                  <Typography fontWeight={500}>{row.name}</Typography>
                 </TableCell>
                 <TableCell>
-                  <Typography>
+                  <Stack direction="row" alignItems="center" spacing={1}>
+                    <Typography fontWeight={500} variant="h6">
+                      {getOrdinalSuffix(place24_1 + 1)}
+                    </Typography>
+                    <Typography fontWeight={500} variant="body2">
+                      ({data24_1?.finalScore} pts)
+                    </Typography>
+                  </Stack>
+                  <Typography fontWeight={500}>
                     {row["24.1"].completed
                       ? row["24.1"].time
-                      : row["24.1"].reps}
+                      : `${row["24.1"].reps} reps`}
                   </Typography>
                 </TableCell>
                 <TableCell>
-                  <Typography>{row["24.2"].score}</Typography>
+                  <Stack direction="row" alignItems="center" spacing={1}>
+                    <Typography fontWeight={500} variant="h6">
+                      {getOrdinalSuffix(place24_2 + 1)}
+                    </Typography>
+                    <Typography fontWeight={500} variant="body2">
+                      ({data24_2?.finalScore} pts)
+                    </Typography>
+                  </Stack>
+                  <Typography fontWeight={500}>{row["24.2"].score}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>-</Typography>
                 </TableCell>
                 <TableCell>
                   <Typography>-</Typography>
